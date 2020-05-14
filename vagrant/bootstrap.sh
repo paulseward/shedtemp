a3a2add2bf4ba30e0a1bedb7fc61ec02d55190a3
Paul Seward-Prior <paul.seward-prior@basekit.com>
2020-05-14 17:28:03 +0100

    Vagrant: Enable bridged networking and avahi discovery

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 sudo apt-get update -y
-sudo apt-get install -y prometheus
+sudo apt-get install -y prometheus avahi-utils
 sudo cp /vagrant/prometheus.yml /etc/prometheus/prometheus.yml
+sudo mv /home/vagrant/avahi-discover.sh /usr/local/bin/avahi-discover.sh
+sudo mkdir -p /etc/prometheus/file_sd
+echo "* * * * * root /usr/local/bin/avahi-discover.sh > /etc/prometheus/file_sd/avahi.yml" | sudo tee -a /etc/crontab
+sudo /usr/local/bin/avahi-discover.sh > /etc/prometheus/file_sd/avahi.yml
 sudo service prometheus restart
 
